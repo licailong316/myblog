8d06cae3d1b81b4b8d462b26a8ce4fabf40c5dff
licailong <3033584702@qq.com>
2024-12-11 16:02:26 +0800

    注册Comment到custom_site

--- myblog/comment/admin.py
+++ myblog/comment/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
+from myblog.custom_site import custom_site
 from .models import Comment
 # from typeidea.base_admin import BaseOwnerAdmin
 
 
 # Register your models here.
-@admin.register(Comment)
+@admin.register(Comment, site=custom_site)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('target', 'nickname', 'content', 'website', 'created_time')
