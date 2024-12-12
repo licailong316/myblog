b812a533a60d0793d490a1a111433e79a437d8e1
licailong <3033584702@qq.com>
2024-12-12 15:34:56 +0800

    封装并配置Sidebar

--- myblog/config/models.py
+++ myblog/config/models.py
@@ -69,4 +69,29 @@ class SideBar(models.Model):
         """获取侧边栏数据"""
         return cls.objects.filter(status=cls.STATUS_SHOW)
 
+    @property
+    def content_html(self):
+        """直接渲染模板"""
+        from blog.models import Post
+        from comment.models import Comment
+        result = ''
+        if self.display_type == self.DISPLAY_HTML:
+            result = self.content
+        elif self.display_type == self.DISPLAY_LATEST:
+            context = {
+                'posts': Post.latest_posts()
+            }
+            result = render_to_string('config/blocks/sidebar_posts.html', context)
+        elif self.display_type == self.DISPLAY_HOT:
+            context = {
+                'posts': Post.hot_posts()
+            }
+            result = render_to_string('config/blocks/sidebar_posts.html', context)
+        elif self.display_type == self.DISPLAY_COMMENT:
+            context = {
+                'comments': Comment.objects.filter(status=Comment.STATUS_NORMAL)
+            }
+            result = render_to_string('config/blocks/sidebar_comments.html', context)
+        return result
+
 
