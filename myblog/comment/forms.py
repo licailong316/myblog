cb121f70f9dd3d796bc94480594f6a15dbc5cc55
licailong <3033584702@qq.com>
2024-12-14 19:29:01 +0800

    给评论增加markdown功能并关闭模板的自动转码

--- myblog/comment/forms.py
+++ myblog/comment/forms.py
@@ -1,3 +1,4 @@
+import mistune
 from django import forms
 
 from .models import Comment
@@ -30,6 +31,7 @@ class CommentForm(forms.ModelForm):
         content = self.cleaned_data.get('content')
         if len(content) < 10:
             raise forms.ValidationError("内容长度怎么能这么短！！")
+        content = mistune.markdown(content)
         return content
 
     class Meta:
